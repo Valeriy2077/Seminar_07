af08e6a204dc2321574f81a1b8ddb88c08cd4837
Valeriy2077 <84186810+Valeriy2077@users.noreply.github.com>
2023-02-25 00:13:09 +0300

    task47

--- Task47/task47.cs
+++ Task47/task47.cs
@@ -7,3 +7,42 @@
 // 1 -3,3 8 -9,9
 
 // 8 7,8 -7,1 9
+
+int[,] FillMatrix(int rows, int cols)
+
+{
+    Random rand = new Random();
+    int [,] mat = new int[rows, cols];
+
+    for(int i = 0; i < rows; i++)
+{
+    for(int j= 0; j < cols; j++)
+    {
+        mat[i,j] = rand.Next(0,100);
+    }
+}
+
+    return mat;
+
+}
+
+void PrintMatrix(int[,] mat)
+{
+
+for(int i = 0; i < mat.GetLength(0); i++)
+{
+    for(int j= 0; j < mat.GetLength(1); j++)
+    {
+        System.Console.Write(mat[i,j] + "\t");
+    }
+
+    System.Console.WriteLine();
+}
+
+}
+System.Console.WriteLine("Введите m и n: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] mat = FillMatrix (m,n);
+PrintMatrix(mat);
+
